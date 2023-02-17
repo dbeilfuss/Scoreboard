880e37c12a21a2186e03c37b2b36a6ae5fa9e942
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-02-16 17:36:14 -0700

    Combine Fractal Landscape Template & Themes into a single database file.

--- ScoreBoard/Model/Themes/Fractal Landscape/FractalLandscape.swift
+++ ScoreBoard/Model/Themes/Fractal Landscape/FractalLandscape.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class FractalLandscape {
     
+    //MARK: - Template Theme
     var templateTheme: Theme
-    
     init () {
         templateTheme = Theme(
             
@@ -44,6 +44,148 @@ class FractalLandscape {
             darkMode: true
             
         )
+    }
+    
+    //MARK: - Themes List
+    
+    var themeList: [Theme] {[
+        fLTheme1,
+        fLTheme2,
+        fLTheme3,
+        fLTheme4,
+        fLTheme5,
+        fLTheme6,
+        fLTheme7,
+        fLTheme8,
+        fLTheme9,
+        fLTheme10,
+        fLTheme11,
+        fLTheme12,
+        fLTheme13,
+        fLTheme14,
+        fLTheme15,
+        fLTheme16,
+        fLTheme17
+    ]}
+    
+    //MARK: - Themes Actual
+    var fLTheme1: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Blue Steel"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeBlue1")!
+        return modifiedTheme
+    }
+    
+    var fLTheme2: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Azure"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeBlue2")!
+        return modifiedTheme
+    }
+    
+    var fLTheme3: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Cobalt"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeBlue3")!
+        return modifiedTheme
+    }
+    
+    var fLTheme4: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Periwinkle"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeBlue4")!
+        return modifiedTheme
+    }
+    
+    var fLTheme5: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Fire"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeFire1")!
+        return modifiedTheme
+    }
+    
+    var fLTheme6: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Emerald"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeGreen1")!
+        return modifiedTheme
+    }
+    
+    var fLTheme7: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Hunter"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeGreen2")!
+        return modifiedTheme
+    }
+    
+    var fLTheme8: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Seafoam"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeGreen3")!
+        return modifiedTheme
+    }
+    
+    var fLTheme9: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Mint"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeGreen4")!
+        return modifiedTheme
+    }
+    
+    var fLTheme10: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Ocean Sky"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeOcean1")!
+        return modifiedTheme
+    }
+    
+    var fLTheme11: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Dolphin"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeOcean2")!
+        return modifiedTheme
+    }
+    
+    var fLTheme12: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Saphire"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeOcean3")!
+        return modifiedTheme
+    }
+    
+    var fLTheme13: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Purple Rain"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapePurple1")!
+        return modifiedTheme
+    }
+    
+    var fLTheme14: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Amethyst"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapePurple2")!
+        return modifiedTheme
+    }
+    
+    var fLTheme15: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Wisteria"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapePurple3")!
+        return modifiedTheme
+    }
+    
+    var fLTheme16: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Alabaster"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeWhite1")!
+        return modifiedTheme
+    }
+    
+    var fLTheme17: Theme {
+        var modifiedTheme: Theme = templateTheme
+        modifiedTheme.name = "Ivory"
+        modifiedTheme.backgroundImage = UIImage.init(named: "fractalLandscapeWhite2")!
+        return modifiedTheme
     }
     
 }
--- ScoreBoard/Model/Themes/ThemesDatabase.swift
+++ ScoreBoard/Model/Themes/ThemesDatabase.swift
@@ -22,25 +22,7 @@ class ThemesDatabase {
     ]}
     
     
-    let fractalLandscapeThemeSet: [Theme] = [
-        FractalLandscapeBlue1().theme,
-        FractalLandscapeBlue2().theme,
-        FractalLandscapeBlue3().theme,
-        FractalLandscapeBlue4().theme,
-        FractalLandscapeFire1().theme,
-        FractalLandscapeGreen1().theme,
-        FractalLandscapeGreen2().theme,
-        FractalLandscapeGreen3().theme,
-        FractalLandscapeGreen4().theme,
-        FractalLandscapeOcean1().theme,
-        FractalLandscapeOcean2().theme,
-        FractalLandscapeOcean3().theme,
-        FractalLandscapePurple1().theme,
-        FractalLandscapePurple2().theme,
-        FractalLandscapePurple3().theme,
-        FractalLandscapeWhite1().theme,
-        FractalLandscapeWhite2().theme
-    ]
+    let fractalLandscapeThemeSet: [Theme] = FractalLandscape().themeList
     
     let cardsAndBoardGames: [Theme] = [
         Railway().theme
