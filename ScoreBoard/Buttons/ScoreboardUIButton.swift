c88fc14e9903aa4c2ecfecfa7936314ad9ee75c2
dbeilfuss <46783350+dbeilfuss@users.noreply.github.com>
2024-08-08 07:16:13 -0600

    add shadow to ScoreboardUIButtons
    To enhance visibility on light backgrounds

--- ScoreBoard/Buttons/ScoreboardUIButton.swift
+++ ScoreBoard/Buttons/ScoreboardUIButton.swift
@@ -16,6 +16,14 @@ class ScoreboardUIButton: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = .none
+        setupShadow()
+        
+        func setupShadow() {
+            self.layer.shadowColor = UIColor.black.cgColor
+            self.layer.shadowOpacity = 0.7
+            self.layer.shadowOffset = CGSize(width: 0, height: 0)
+            self.layer.shadowRadius = 15
+        }
     }
     
     func setupButton(state: ScoreboardState, theme: Theme) {
